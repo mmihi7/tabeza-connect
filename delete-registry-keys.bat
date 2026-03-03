5583c987d251887b80c3df7eb41616f203ea296a
Henry Mwenesi <conversationapps@gmail.com>
2026-03-03 12:18:15 +0300

    Cleanup workspace

--- delete-registry-keys.bat
+++
@@ -1,5 +0,0 @@
-@echo off  
-echo Deleting TabezaConnect registry keys...  
-reg delete "HKLM\SOFTWARE\Tabeza\TabezaConnect" /f  
-if 0 equ 0 (echo Registry keys deleted successfully) else (echo Failed to delete registry keys - make sure you run as Administrator)  
-pause 
